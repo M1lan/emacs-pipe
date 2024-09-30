3074d845277a4de4d1dd7eb3aed723f0d900f162
Milan Santosi <milan.santosi@gmail.com>
2024-09-30 20:33:57 +0200

    update prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::temp_dir;
+use std::fs::{File, remove_file};
+use std::io::{self, Read, Write};
+use std::process::{Command, Stdio};
+use md5;
+
+fn main() -> io::Result<()> {
+    let mut input = String::new();
+
+    // Check if input is coming from stdin
+    if atty::isnt(atty::Stream::Stdin) {
+        io::stdin().read_to_string(&mut input)?;
+    }
+
+    if input.is_empty() {
+        return Ok(());
+    }
+
+    let hash = format!("{:x}", md5::compute(&input));
+    let file_path = temp_dir().join(format!("{}.emacs-pager", hash));
+
+    {
+        let mut file = File::create(&file_path)?;
+        file.write_all(input.as_bytes())?;
+    }
+
+    println!("reading into emacs...");
+    Command::new("emacsclient")
+        .arg(file_path.to_str().unwrap())
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .status()?;
+
+    remove_file(file_path)?;
+    Ok(())
 }
